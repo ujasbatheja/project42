138823f755abeb1fbdea9c82ef139c0b198c129a
ujasbatheja <71878150+ujasbatheja@users.noreply.github.com>
2021-01-14 18:59:51 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,10 @@
+class Drop{
+    constructor(){
+        this.position.x = 200;
+        this.position.y = 200;
+    }
+    diaplay(){
+        droups = creatwsprite(100,100,10,10);
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,23 @@
+//var man, manImage;
+//var thunderImage1, thunderImage2, thunderImage3, thunderImage4;
+var drops;
+
+function preload(){
+    //manImage = loadImage("images/Walking Frame/walking_1.png");
+
+    //thunderImage1 = loadImage("images/thunderbolt/1.png");
+    //thunderImage2 = loadImage("images/thunderbolt/2.png");
+    //thunderImage3 = loadImage("images/thunderbolt/3.png");
+    //thunderImage4 = loadImage("images/thunderbolt/4.png");
+}
+
+function setup(){
+    createCanvas(400,700);
+    
+
+}
+
+function draw(){
+    
+}   
+
